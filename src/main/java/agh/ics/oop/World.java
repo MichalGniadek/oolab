23f9ede8136f3a97134322f9a642919a888522da
Michał Gniadek <mgniadek@interia.pl>
2021-10-16 20:25:56 +0200

    Cleanup

--- src/main/java/agh/ics/oop/World.java
+++ src/main/java/agh/ics/oop/World.java
@@ -14,20 +14,6 @@ public class World {
     }
 
     public static Direction[] convert(String[] args) {
-//        Direction[] directions = new Direction[args.length];
-//        int i = 0;
-//        for(String arg : args){
-//            directions[i] =  switch(arg){
-//                case "f" -> Direction.FORWARD;
-//                case "b" -> Direction.BACKWARD;
-//                case "r" -> Direction.RIGHT;
-//                case "l" -> Direction.LEFT;
-//                default -> null;
-//            };
-//            i++;
-//        }
-//        return directions;
-
         return Arrays.stream(args).map(arg -> switch (arg) {
             case "f" -> Direction.FORWARD;
             case "b" -> Direction.BACKWARD;
@@ -38,29 +24,6 @@ public class World {
     }
 
     public static void run(Direction[] args) {
-//        if(input.length > 0){
-//            out.print(input[0]);
-//            for(String inp : Arrays.copyOfRange(input, 1, input.length)){
-//                out.print(", ");
-//                out.print(inp);
-//            }
-//            out.println();
-//        }
-
-//        for (Direction arg : args) {
-//            String message = switch (arg) {
-//                case FORWARD -> "Zwierzak idzie do przodu";
-//                case BACKWARD -> "Zwierzak idzie do tyłu";
-//                case RIGHT -> "Zwierzak skręca w prawo";
-//                case LEFT -> "Zwierzak skręca w lewo";
-//                default -> "Nieznana komenda";
-//            };
-//
-//            if (!message.equals("Nieznana komenda")) {
-//                out.println(message);
-//            }
-//        }
-
         Arrays.stream(args)
                 .filter(Objects::nonNull)
                 .map(arg -> switch (arg) {
