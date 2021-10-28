5ef43b72ff6189a0fcf2a73a599711e3116b0e7f
Michał Gniadek <mgniadek@interia.pl>
2021-10-28 14:16:55 +0200

    Animal collisions

--- src/main/java/agh/ics/oop/Animal.java
+++ src/main/java/agh/ics/oop/Animal.java
@@ -1,11 +1,22 @@
 package agh.ics.oop;
 
+import java.util.HashSet;
+
 public class Animal {
     private MapDirection direction = MapDirection.NORTH;
-    private Vector2d position = new Vector2d(2,2);
+    private Vector2d position = new Vector2d(2, 2);
 
-    public void move(MoveDirection direction){
-         switch (direction){
+    private static HashSet<Vector2d> occupied = new HashSet<Vector2d>();
+
+    public Animal() {
+        if (occupied.contains(position)) {
+            throw new IllegalStateException();
+        }
+        occupied.add(position);
+    }
+
+    public void move(MoveDirection direction) {
+        switch (direction) {
             case FORWARD -> tryMoveTo(position.add(this.direction.toUnitVector()));
             case BACKWARD -> tryMoveTo(position.add(this.direction.toUnitVector().opposite()));
             case RIGHT -> this.direction = this.direction.next();
@@ -13,13 +24,17 @@ public class Animal {
         }
     }
 
-    private void tryMoveTo(Vector2d new_pos){
-        if(new_pos.follows(new Vector2d(0,0)) && new_pos.precedes(new Vector2d(4,4))){
+    private void tryMoveTo(Vector2d new_pos) {
+        if (new_pos.follows(new Vector2d(0, 0)) &&
+                new_pos.precedes(new Vector2d(4, 4)) &&
+                !occupied.contains(new_pos)) {
+            occupied.remove(position);
+            occupied.add(new_pos);
             position = new_pos;
         }
     }
 
-    public boolean isAt(Vector2d position){
+    public boolean isAt(Vector2d position) {
         return this.position.equals(position);
     }
 
--- src/main/java/agh/ics/oop/World.java
+++ src/main/java/agh/ics/oop/World.java
@@ -5,11 +5,20 @@ import java.util.Objects;
 
 public class World {
     public static void main(String[] args) {
+        // Animal collisions
+//        var a1 = new Animal();
+//        a1.move(MoveDirection.FORWARD);
+//        var a2 = new Animal();
+//        System.out.println(a1);
+//        a2.move(MoveDirection.FORWARD);
+//        System.out.println(a1);
+
+        // Lab 3
         var animal = new Animal();
         var directions = OptionsParser.parse(args);
         for(var dir : directions){
             animal.move(dir);
-            System.out.print(animal);
+            System.out.println(animal);
         }
     }
 }
